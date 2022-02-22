1b72a09e7d5966aa48a7273d12a53bf9feb5181a
Grencione <76771267+Grencione@users.noreply.github.com>
2022-02-22 13:40:54 +0200

    Update projekts.py

--- projekts.py
+++ projekts.py
@@ -4,3 +4,30 @@ x=sqrt(9)+2**5
 y=10+x
 print(y)
 
+1 Burke Elza
+
+2 Dundurs Mārcis
+
+3 Dūrēja Annemarija
+
+4 Dzērve Artūrs
+
+5 Greilihs Dīvs Rihards
+
+6 Lange Rūdolfs Toms
+
+7 Meškonis Aleksandrs Nils
+
+8 Robežnieks Eduards
+
+9 Saukums Krists
+
+10 Sprūģis Gabriels
+
+11 Stankevičs Viesturs
+
+12 Strazds Emīls
+
+13 Škutāne Ieva
+
+14 Zīle Kristaps 
